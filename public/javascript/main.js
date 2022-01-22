4538a7ef9d2bfaba332deaf110fcaa28b54af8c2
nazimudheen267 <nazimudheen267@gmail.com>
2022-01-22 16:47:05 +0530

    aside section added and post section style changed

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,13 +4,29 @@
     const menuBtn = document.getElementById('js-menu-btn');
     const menu = document.getElementById('js-menu');
     const body = document.querySelector('body');
+    const time = document.getElementById('js-time')
+    const date = document.getElementById('js-date')
+    const device = document.getElementById('js-device')
 
     // listening a event when a user clicking the button
-    menuBtn.addEventListener('click', () => {
-        menu.style.display = 'block';
-    })
+    // menuBtn.addEventListener('click', () => {
+    //     menu.style.display = 'block';
+    // })
 
-    document.querySelector('.overlay').addEventListener('click', () => {
-        console.log('clicked');
-        menu.style.display = 'none';
-    })
+    // document.querySelector('.overlay').addEventListener('click', () => {
+    //     console.log('clicked');
+    //     menu.style.display = 'none';
+    // })
+
+    const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', "Dec"]
+
+    const dt = new Date();
+    const time = dt.getHours() > 12 ? dt.getHours() - 12 : dt.getHours()
+
+    time.innerHTML = `${time}: ${dt.getMinutes()} ${time > 12 ? 'PM': 'AM'}`
+    date.innerHTML = `${month[dt.getMonth()]} ${dt.getDate()}, ${dt.getFullYear()}`
+
+    
+
+
+    
